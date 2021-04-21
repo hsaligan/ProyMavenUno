eefcf74d0adb3d5dc1e8eadbeb059cde9e0d59e1
Herbert <beto_sach@hotmail.com>
2021-04-21 12:25:13 -0500

    Primer version

---
+++ src/main/java/com/saligan/ejemmaven/App.java
@@ -0,0 +1,13 @@
+package com.saligan.ejemmaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
